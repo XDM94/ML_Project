ea5fe4fa63c887620e03d7456bec649772f14406
XDM94 <sturmtrooper94@gmail.com>
2023-12-17 17:06:14 +0300

    Change app_g

--- denis_khlamov/gpt_clone/app_g.py
+++ denis_khlamov/gpt_clone/app_g.py
@@ -21,6 +21,7 @@ if prompt := st.chat_input("Спрашивай то что хочешь узна
     with st.chat_message("user"):
         st.markdown(prompt)
 
+    st.file_uploader("Choose an image:", type=["jpg", "jpeg", "png"])
     with st.chat_message("assistant"):
         message_placeholder = st.empty()
         full_response = ""
